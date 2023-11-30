77c2ef45001b97266afe796cfde71018742dd33d
Balaji.K <balajiraghu159002@gmail.com>
2023-11-30 11:40:59 +0530

    inti files

---
+++ navbar.js
@@ -0,0 +1,30 @@
+
+
+window.addEventListener('load', function () {
+    const contentDiv = document.getElementById('navbar');
+    contentDiv.innerHTML=`<img src="https://cdn-icons-png.flaticon.com/128/2917/2917995.png" style="width:48px;padding-left:5px;padding-top:5px"><p class="imgtext">MALAI CHIRAGAN</p></img>
+                <a href="about.html">
+                    <div class="icon">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i>  
+                    </div>
+                    <div class="name"><span data-text="About">About</span></div>
+                </a>
+                <a href="contact.html">
+                    <div class="icon">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>              
+                    </div>
+                    <div class="name"><span data-text="Contact">Contact</span></div>
+                </a>
+                <a href="#">
+                    <div class="icon">
+                        <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>        
+                    </div>
+                    <div class="name"><span data-text="Cart">Cart</span></div>
+                </a>
+                <a href="index.html">
+                    <div class="icon">
+                        <i class="fa fa-home" aria-hidden="true"></i>     
+                    </div>
+                    <div class="name"><span data-text="Home">Home</span></div>
+                </a>`;
+});
